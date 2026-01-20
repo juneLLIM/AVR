52519ace279cb0f6a881a6a89e8e39fe2380f962
June <jooeun9199@gmail.com>
2026-01-20 16:09:21 +0000

    Add RAF settings

--- scripts.sh
+++ scripts.sh
@@ -1,2 +1,4 @@
-CUDA_VISIBLE_DEVICES=1 nohup python avr_runner.py &
+CUDA_VISIBLE_DEVICES=1 nohup python avr_runner.py --config config_files/avr_raf_empty.yml --dataset_dir ~/data/dataset/raf_dataset/EmptyRoom > /dev/null &
+CUDA_VISIBLE_DEVICES=1 nohup python avr_runner.py --config config_files/avr_raf_furnished.yml --dataset_dir ~/data/dataset/raf_dataset/FurnishedRoom > /dev/null &
 CUDA_VISIBLE_DEVICES=1 nohup python infer.py --ckpt logs/MeshRIR/Meshrir_sliced_0.1s/ckpts/500000.tar &
+tensorboard --logdir tensorboard_logs
